951874f91b3af4fdb450ada82861f129e8f0a2e8
Ronan Wood <ronan@cocoatoa.com>
2017-07-30 11:40:07 -0700

    code machin broke. I tried to do a thing in player class and it BROKe

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -1,4 +1,23 @@
 package classes;
 
 public class Player {
+	// placeholder boolean, replace later
+	boolean gameStartedplaceholder = false;
+
+	private int playerX;
+	private int playerY;
+
+	int playerXStart;
+	int playerYStart;
+
+	void initializePlayer() {
+		playerX = playerXStart;
+		playerY = playerYStart;
+	}
+
+	public static void main(String[] args) {
+		while (gameStartedplaceholder == true) {
+
+		}
+	}
 }
